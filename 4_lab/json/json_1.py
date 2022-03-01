5893bd6ad6e98b75f21900c62eaa4798dd4909e4
Merey Polatkhan <m_polatkhan@kbtu.kz>
2022-03-01 12:06:03 +0600

    4_lab: date; math; gen; json

--- 4_lab/json/json_1.py
+++ 4_lab/json/json_1.py
@@ -1,17 +1,22 @@
 import json 
 
 with open('sample-data.json', 'r') as f:
-    data = f.read() 
-
-a = json.loads(data)
+    a = json.load(f)
 
 # print(type(a)) --> dict 
 
-print("Interface Status")
-print('=' * 80)
+with open('output.txt', 'w') as out:
+    print("Interface Status", file = out)
+    print('=' * 80, file = out)
+    print("DN                                                 Description           Speed    MTU  ", file = out)
+    print("-------------------------------------------------- --------------------  ------  ------", file = out) 
+    for i in a['imdata']:
+        dn = i['l1PhysIf']['attributes']['dn']
+        descr = i['l1PhysIf']['attributes']['descr']
+        speed = i['l1PhysIf']['attributes']['speed']
+        mtu = i['l1PhysIf']['attributes']['mtu']
+        print(f'{dn}\t\t\t\t\t\t{descr}\t\t{speed}   {mtu}', file = out)
+        
 
-print("DN                                                 Description           Speed    MTU  ")
-print("-------------------------------------------------- --------------------  ------  ------") 
 
-for i in a["imdata"]:
-    pass
+
