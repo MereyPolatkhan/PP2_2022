e804fe327bebf735c78a052ffc4dbb1584ce49a9
Merey Polatkhan <m_polatkhan@kbtu.kz>
2022-04-26 21:45:27 +0600

    PostgrSQL

--- 10_lab/PhoneBook/2.py
+++ 10_lab/PhoneBook/2.py
@@ -10,10 +10,10 @@ new_contacts = [
     [9, 'contact 9', 'contact9@gmail.com', '87078622899', 'Company 9'],
     [10, 'contact 10', 'contact10@gmail.com', '87078622810', 'Company 10'],
     [11, 'contact 11', 'contact11@gmail.com', '87078622811', 'Company 11'],
-    [10, 'contact 12', 'contact12@gmail.com', '87078622812', 'Company 12'],
-    [10, 'contact 13', 'contact13@gmail.com', '87078622813', 'Company 13'],
-    [10, 'contact 14', 'contact14@gmail.com', '87078622814', 'Company 14'],
-    [10, 'contact 15', 'contact15@gmail.com', '87078622815', 'Company 15'],
+    [12, 'contact 12', 'contact12@gmail.com', '87078622812', 'Company 12'],
+    [13, 'contact 13', 'contact13@gmail.com', '87078622813', 'Company 13'],
+    [14, 'contact 14', 'contact14@gmail.com', '87078622814', 'Company 14'],
+    [15, 'contact 15', 'contact15@gmail.com', '87078622815', 'Company 15'],
 
 ]
 with open('new_contacts.csv', 'w') as f:
